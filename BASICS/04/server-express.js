16994cefb1ab497e72063b6273c2c156786fcf4e
sdhan <hsd970730@naver.com>
2025-03-29 18:25:11 +0900

    [DOIT] express 라우팅
    - put, post, delete, get

--- BASICS/04/server-express.js
+++ BASICS/04/server-express.js
@@ -10,6 +10,28 @@ app.get("/test", (req, res) => {
     res.send("Test Node")
 });
 
+app.post("/test", (req, res) => {
+    res.send("Post Node")
+});
+
+app.get("/test/:id", (req, res) => {
+    res.send(`Route Node GET:id => ${req.params.id}`)
+});
+
+app.use(express.text());
+app.use(express.json());
+app.post("/test/:id", (req, res) => {
+    res.send(`Route Node POST:id => ${req.params.id}, ${req.body.id}`)
+});
+
+app.put("/test/:id", (req, res) => {
+    res.send(`Route Node PUT:id => ${req.params.id}`)
+});
+
+app.delete("/test/:id", (req, res) => {
+    res.send(`Route Node DELETE:id => ${req.params.id}`)
+});
+
 app.listen(3010, () => {
     console.log("Express Server Start!")
 });
