b88faeca9ceda7c8b9af549c885cc0f42ee2205f
sdhan <hsd970730@naver.com>
2025-03-31 18:56:08 +0900

    [DOIT] express 미들웨어 구현
    - Raw (Binary)
    - Text (Plain-Text)
    - urlEncoding (x-form-encoding)
    - Json (JSON)

--- BASICS/04/server-express.js
+++ BASICS/04/server-express.js
@@ -1,37 +1,39 @@
 import express from "express"
 
-const app = express();
+const router = express();
 
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
     res.send("Hello Node")
 });
 
-app.get("/test", (req, res) => {
+router.get("/test", (req, res) => {
     res.send("Test Node")
 });
 
-app.post("/test", (req, res) => {
+router.post("/test", (req, res) => {
     res.send("Post Node")
 });
 
-app.get("/test/:id", (req, res) => {
+router.get("/test/:id", (req, res) => {
     res.send(`Route Node GET:id => ${req.params.id}`)
 });
 
-app.use(express.text());
-app.use(express.json());
-app.post("/test/:id", (req, res) => {
+router.use(express.raw());
+router.use(express.urlencoded({ extended: true }));
+router.use(express.text());
+router.use(express.json());
+router.post("/test/:id", (req, res) => {
     res.send(`Route Node POST:id => ${req.params.id}, ${req.body.id}`)
 });
 
-app.put("/test/:id", (req, res) => {
+router.put("/test/:id", (req, res) => {
     res.send(`Route Node PUT:id => ${req.params.id}`)
 });
 
-app.delete("/test/:id", (req, res) => {
+router.delete("/test/:id", (req, res) => {
     res.send(`Route Node DELETE:id => ${req.params.id}`)
 });
 
-app.listen(3010, () => {
+router.listen(3010, () => {
     console.log("Express Server Start!")
 });
